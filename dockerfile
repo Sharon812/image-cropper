67ff1b5557d916d463c2600a6a98a606c243b01d
Sharon812 <sharusharon1110@gmail.com>
2025-02-19 21:32:38 +0530

    implemented docker file

---
+++ dockerfile
@@ -0,0 +1,16 @@
+FROM node:23.8.0
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+ENV PORT=5000
+
+EXPOSE 5000
+
+CMD [ "npm","start" ]
+
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const env = require("dotenv").config();
 const app = express();
-const port = 3000;
 
 // Serve static files from Views directory
-app.use(express.static(path.join(__dirname, 'Views')));
+app.use(express.static(path.join(__dirname, "Views")));
 
 // Serve the main page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Views', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "Views", "index.html"));
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server running at`);
 });
